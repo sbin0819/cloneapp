931bbebcc0f77415ff7b4aed2a63a4cb2f923d2b
sbin0819 <sbinha123@gmail.com>
2022-02-01 22:38:01 +0900

    some changed

--- carrot-market/pages/profile/bought.tsx
+++ carrot-market/pages/profile/bought.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
+const Bought: NextPage = () => {
   return (
     <div className="flex flex-col space-y-5 py-10">
       {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
         <div
           key={i}
-          className="flex px-4 border-b pb-4 cursor-pointer justify-between"
+          className="flex px-4  border-b pb-5 cursor-pointer justify-between"
         >
-          <div className="flex space-x-4 ">
+          <div className="flex space-x-4">
             <div className="w-20 h-20 bg-gray-400 rounded-md" />
             <div className="pt-2 flex flex-col">
               <h3 className="text-sm font-medium text-gray-900">
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           <div className="flex space-x-2 items-end justify-end">
-            <div className="flex space-x-0.5 items-center text-sm text-gray-600">
+            <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
               <svg
                 className="w-4 h-4"
                 fill="none"
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
               </svg>
               <span>1</span>
             </div>
-            <div className="flex space-x-0.5 items-center text-sm text-gray-600">
+            <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
               <svg
                 className="w-4 h-4"
                 fill="none"
@@ -56,25 +56,8 @@ const Home: NextPage = () => {
           </div>
         </div>
       ))}
-      <button className="fixed hover:bg-orange-500 transition-colors cursor-pointer bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white">
-        <svg
-          className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
-      </button>
     </div>
   );
 };
 
-export default Home;
+export default Bought;
