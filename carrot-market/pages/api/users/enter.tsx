4dfcd14a414f5d516beab006d290b6cc8b9fd674
sbin0819 <sbinha123@gmail.com>
2022-02-07 21:02:46 +0900

    feat: forms

--- carrot-market/pages/api/users/enter.tsx
+++ carrot-market/pages/api/users/enter.tsx
@@ -1,10 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-// import client from '@libs/client'
+import withHandler from '@libs/server/withHandler';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  console.log(res);
-  res.status(200).end();
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
+  return res.status(200).end();
 }
+
+export default withHandler('POST', handler);
