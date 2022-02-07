876f6d5c9bb3546f7a1a72644746a892ca9ffa4e
sbin0819 <sbinha123@gmail.com>
2022-02-07 23:27:05 +0900

    feat: email, phone

--- carrot-market/libs/server/withHandler.ts
+++ carrot-market/libs/server/withHandler.ts
@@ -1,4 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+
+export interface ResponseType {
+  ok: boolean;
+  [key: string]: any;
+}
 export default function withHandler(
   method: 'GET' | 'POST' | 'DELETE',
   fn: (req: NextApiRequest, res: NextApiResponse) => void,
--- carrot-market/pages/api/users/enter.tsx
+++ carrot-market/pages/api/users/enter.tsx
@@ -1,9 +1,57 @@
+import mail from '@sendgrid/mail';
+import twilio from 'twilio';
 import { NextApiRequest, NextApiResponse } from 'next';
-import withHandler from '@libs/server/withHandler';
+import client from '@libs/server/index';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  return res.status(200).end();
+mail.setApiKey(process.env.SENDGRID_KEY as string);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
+
+  if (!user) return res.status(400).json({ ok: false });
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: 'Anonymous',
+            ...user,
+          },
+        },
+      },
+    },
+  });
+  if (phone) {
+    await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}.`,
+    });
+  } else if (email) {
+    const email = await mail.send({
+      from: 'sbinha123@gmail.com',
+      to: 'sbinha123@gmail.com',
+      subject: 'Your Carrot Market Verification EMAIL',
+      text: `Your token is ${payload}`,
+      html: `<h1>hi ${payload}</h1>`,
+    });
+    console.log(email);
+  }
+  return res.json({
+    ok: true,
+    ...token,
+  });
 }
 
 export default withHandler('POST', handler);
