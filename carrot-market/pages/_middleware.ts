ad7c436314be3de99e3d98a196b2a284fb6e9ff5
sbin0819 <sbinha123@gmail.com>
2022-02-25 16:41:28 +0900

    feat: revalidate

--- carrot-market/pages/_middleware.ts
+++ carrot-market/pages/_middleware.ts
@@ -4,10 +4,10 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.ua?.isBot) {
     return new Response("Plz don't be a btoa.", { status: 403 });
   }
-  if (!req.url.includes('/api')) {
-    if (!req.cookies.carrotsession && !req.url.includes('/enter')) {
-      return NextResponse.redirect('/enter');
-    }
-  }
+  // if (!req.url.includes('/api')) {
+  //   if (!req.cookies.carrotsession && !req.url.includes('/enter')) {
+  //     return NextResponse.redirect('/enter');
+  //   }
+  // }
   // return NextResponse.json({ ok: true });
 }
--- carrot-market/pages/api/posts/index.ts
+++ carrot-market/pages/api/posts/index.ts
@@ -24,6 +24,7 @@ async function handler(
         },
       },
     });
+    await res.unstable_revalidate('/community');
     res.json({
       ok: true,
       post,
